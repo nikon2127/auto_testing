56fd37b0fd49e945eed4d266a4b655e4e3566928
@nikon2127 <nikon2127@gmail.com>
2022-07-05 15:04:18 +0300

    dd

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,13 +1,7 @@
 from auto_testing.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('function not work')
-
-if capitalize('') != '':
-    raise Exception('function not work not args')
 
 def test_capitalize():
     assert capitalize('hello') == 'Hello'
     assert capitalize('') == ''
-
-print('capitalize work')
+    assert capitalize(' hexlet') == ' hexlet'
